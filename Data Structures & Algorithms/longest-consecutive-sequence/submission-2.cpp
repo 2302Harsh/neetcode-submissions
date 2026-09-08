9e95d0204dc5757d9178dcb98ffa3c8ec61b0900
2302Harsh <98223547+2302Harsh@users.noreply.github.com>
2026-09-08 23:03:18 +1000

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,33 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for(int num : nums){
+            seen.insert(num);
+        }
+
+        int ans =0;
+
+        for(int num : nums){
+            if(seen.find(num-1) == seen.end()){
+                int n = num;
+                int count = 0;
+                while(seen.find(n) != seen.end()){
+                    count++;
+                    n++;
+                }
+
+                ans = max(ans, count);
+            }
+        }
+
+        return ans;
+    }
+};
